d5bfea59cb89800327bdfebc870bb528d9457f04
Dragan <dragan.ilisic@yahoo.com>
2014-06-17 12:01:25 -0400

    initial checkin of sales tool.

---
+++ SalesTool/src/ca/demo/salestool/SalesData.java
@@ -0,0 +1,11 @@
+package ca.demo.salestool;
+
+public class SalesData {
+	int data[] = {0, 4, 42};
+
+	public void display() {
+		// TODO Auto-generated method stub
+		System.out.println("TODO: Implement data display");
+	}
+
+}
